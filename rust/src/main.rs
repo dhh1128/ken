bf5e3c69b560d7aebe813c1941daa47c68e7a5d7
dhh1128 <daniel.hardman@gmail.com>
2022-03-04 20:43:31 +0100

    Add basic cmdline and build info
    Signed-off-by: dhh1128 <daniel.hardman@gmail.com>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,9 @@
-use std::{env, fs};
+use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
+extern crate clap;
 extern crate colored;
 #[macro_use]
 extern crate lazy_static;
@@ -11,6 +12,7 @@ extern crate regex;
 extern crate walkdir;
 
 use regex::Regex;
+use clap::{arg, Arg, Command};
 use colored::*;
 use walkdir::{WalkDir, DirEntry};
 
@@ -23,9 +25,9 @@ fn complain(e: &dyn std::error::Error) {
     println!("  {}: {}", "error".red(), e);
 }
 
-fn transform_file(ken_path: &str) -> bool {
+fn transform_file(ken_path: &PathBuf) -> bool {
     let mut html_path = PathBuf::from(&ken_path);
-    println!("{} {} -> {:?}", "Transforming".green(), ken_path, html_path.to_str());
+    println!("{} {} -> {:?}", "Transforming".green(), ken_path.to_str().expect("bad utf8 path"), html_path.to_str());
     html_path.set_extension("html");
     match File::create(&html_path) {
         Ok(mut html_file) => {
@@ -61,28 +63,58 @@ fn is_ken(entry: &DirEntry) -> bool {
     answer
 }
 
-fn transform_dir(folder: &str) {
+fn transform_dir(folder: &PathBuf) {
     for entry in WalkDir::new(folder).into_iter().filter_map(|e| e.ok()) {
         if is_ken(&entry) {
             if let Some(fname) = entry.file_name().to_str() {
-                transform_file(fname);
+                transform_file(&PathBuf::from(fname));
             }
         }
     }
 }
 
+// Use of a mod or pub mod is not actually necessary.
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        args.push(".".to_string())
-    }
-    for arg in &args[1..] {
-        if let Ok(metadata) = fs::metadata(&arg) {
-            if metadata.is_file() {
-                transform_file(&arg);
-            } else if metadata.is_dir() {
-                transform_dir(&arg);
+
+    let cmdline = Command::new(built_info::PKG_NAME)
+        .about(built_info::PKG_DESCRIPTION)
+        .version(built_info::PKG_VERSION)
+        .author(built_info::PKG_AUTHORS)
+        .subcommand(Command::new("standalone")
+            .about("Generates standalone web pages")
+            .arg(Arg::new("template").short('t'))
+            .arg_required_else_help(true)
+            .arg(arg!(<PATH> ... "files or folders to convert").allow_invalid_utf8(true)));
+
+    let cmd = cmdline.get_matches();
+    match cmd.subcommand() {
+        Some(("standalone", args)) => {
+
+            let mut paths = args
+                .values_of_os("PATH")
+                .unwrap_or_default()
+                .map(PathBuf::from)
+                .collect::<Vec<_>>();
+
+            if paths.len() == 1 {
+                paths.push(PathBuf::from("."))
+            }
+
+            for path in &paths {
+                if let Ok(metadata) = fs::metadata(&path) {
+                    if metadata.is_file() {
+                        transform_file(&path);
+                    } else if metadata.is_dir() {
+                        transform_dir(&path);
+                    }
+                }
             }
         }
+        _ => unreachable!(),
     }
 }
